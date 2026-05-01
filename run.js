881b153c03207a81ce21ecca3a9a65de591e4a78
Minah Kim <kimma1205@gmail.com>
2026-05-01 13:44:53 +0900

    Remove title

--- run.js
+++ run.js
@@ -28,7 +28,6 @@ async function getRandomWord() {
 
 async function sendTelegramMessage(word) {
   const text =
-    `📚 ${new Date().toLocaleDateString('ko-KR', { month: 'long', day: 'numeric', timeZone: 'Asia/Seoul' })}의 단어` +
     `🇰🇷 ${word.korean}\n` +
     `🇯🇵 ${word.japanese}\n` +
     `🇺🇸 ${word.english}`;
